e9bc7bbde1fabcc3ef9b88e31e78110facbc273c
Ogungbe Simisola <ogungbejuliet@gmail.com>
2022-06-24 07:48:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = "Ogungbe Simisola";
+let height = "190.5cm";
+let country = "Nigeria";
+
+
+console.log(name);
+console.log(height);
+console.log(country);
+
+alert(name)
+alert(height)
+alert(country)
+
